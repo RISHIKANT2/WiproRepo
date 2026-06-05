05289b3139cabcd5e4ebce1289befddd2145f544
RISHIKANT2 <rishikantpravesh632@gmail.com>
2026-06-05 12:32:31 +0530

    Initial Spring Boot project setup

---
+++ MetroRide-Java-API/src/test/java/com/metroride/MetroRide_Java_API/MetroRideJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metroride.MetroRide_Java_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetroRideJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
